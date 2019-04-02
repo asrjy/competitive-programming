e5bf237955720717a478d12f4cdcb3eb34ab441f
Yashwanth Allakky <36568239+kahunatamata@users.noreply.github.com>
2019-04-02 20:26:59 +0530

    Create codeforces263a.cpp

---
+++ Codeforces263A/codeforces263a.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+    int i= 0, j= 0, ipos = 0, jpos = 0, beautifulans = 0;
+    int numbers[5][5];
+    for( i = 0; i < 5; i++){ 
+         for(j = 0; j < 5; j++){
+             cin >> numbers[i][j];
+             if (numbers[i][j] == 1){
+                ipos = i;
+                jpos = j;
+            }
+         }
+    }
+    beautifulans = abs(ipos - 2) + abs(jpos - 2);
+    cout << beautifulans;
+    return 0;
+}
